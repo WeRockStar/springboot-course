7e88c65bd815b7d17d1bf103a5c89b325fb365d2
WeRockStar <kotchaphan.m@gmail.com>
2017-03-04 14:54:05 +0700

    Update README.md

--- chapter1/springboot-maven-project/src/main/java/com/sprint3r/deans4j/SpringbootMavenProjectApplication.java
+++ chapter1/springboot-maven-project/src/main/java/com/sprint3r/deans4j/SpringbootMavenProjectApplication.java
@@ -2,9 +2,17 @@ package com.sprint3r.deans4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootMavenProjectApplication {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMavenProjectApplication.class, args);
---
+++ chapter2/springboot-config-client/README.md
@@ -0,0 +1,1 @@
+##### curl -d{} http://localhost:8080/refresh
